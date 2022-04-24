7d6601fafe2b120ebd02c4d73e3498c9b45b606e
alex-1-tech <Alex225Games@yandex.ru>
2022-04-24 17:39:13 +0300

    sqlalchemy

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ import datetime
 from data import db_session
 from data.users import User
 from data.news import News
+from forms.news import NewsForm
 from forms.user import RegisterForm, LoginForm
-from flask_login import LoginManager, login_required, logout_user, login_user
+from flask_login import LoginManager, login_required, logout_user, login_user, current_user
 
 app = Flask(__name__)
 api = Api(app)
@@ -24,7 +25,9 @@ login_manager.init_app(app)
 def show_profile(nickname=None):
     db_sess = db_session.create_session()
     user = db_sess.query(User).filter(User.nickname == nickname).first()
-    return render_template('profile.html', title='Профиль')
+    news = db_sess.query(News).filter(News.user == user).all()
+    return render_template('profile.html', title='Профиль', user=user, news=news)
+
 
 @app.route('/logout')
 @login_required
@@ -41,7 +44,9 @@ def load_user(user_id):
 
 @app.route('/')
 def events():
-    return render_template('event_page.html')
+    db_sess = db_session.create_session()
+    news = db_sess.query(News).filter(News.is_private != True).all()
+    return render_template('event_page.html', news=news)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -85,6 +90,56 @@ def login():
     return render_template('login.html', title='Авторизация', form=form)
 
 
+@app.route('/news', methods=['GET', 'POST'])
+@login_required
+def add_news():
+    form = NewsForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        news = News()
+        news.title = form.title.data
+        news.content = form.content.data
+        news.is_private = form.is_private.data
+        current_user.news.append(news)
+        db_sess.merge(current_user)
+        db_sess.commit()
+        return redirect(f'/profile={current_user.nickname}')
+    return render_template('news.html', title='Добавление новости',
+                           form=form)
+
+
+@app.route('/news/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit_news(id):
+    form = NewsForm()
+    if request.method == "GET":
+        db_sess = db_session.create_session()
+        news = db_sess.query(News).filter(News.id == id,
+                                          News.user == current_user
+                                          ).first()
+        if news:
+            form.title.data = news.title
+            form.content.data = news.content
+            form.is_private.data = news.is_private
+        else:
+            abort(404)
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        news = db_sess.query(News).filter(News.id == id,
+                                          News.user == current_user
+                                          ).first()
+        if news:
+            news.title = form.title.data
+            news.content = form.content.data
+            news.is_private = form.is_private.data
+            db_sess.commit()
+            return redirect(f'/profile={current_user.nickname}')
+        else:
+            abort(404)
+    return render_template('news.html',
+                           title='Редактирование новости',
+                           form=form
+                           )
 
 
 def main():
